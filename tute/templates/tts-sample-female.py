8bc9590d32a3d676a827f5fa1d6ff524f766d6c1
Sneha Supriya <codingsneha@gmail.com>
2023-01-15 22:08:47 +0530

    update files

--- tute/templates/tts-sample-female.py
+++ tute/templates/tts-sample-female.py
@@ -1,7 +1,7 @@
 import pyttsx3
 
 string="""Hello\n
-        this is a sample run, thank you for watching"""
+        this is a sample run"""
 
 engine = pyttsx3.init()
 engine.setProperty('rate', 150)
