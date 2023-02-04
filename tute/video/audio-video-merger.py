4de9d244267f7973e98e958651240ff22e94024b
Sneha Supriya <codingsneha@gmail.com>
2023-02-04 15:32:39 +0530

    update files

--- tute/video/audio-video-merger.py
+++ tute/video/audio-video-merger.py
@@ -1,7 +1,24 @@
+import mysql.connector
 import ffmpeg
 
-input_video = ffmpeg.input('./test/test_video.webm')
+mydb = mysql.connector.connect(
+    host = "localhost",
+    database = "tute"
+)
 
-input_audio = ffmpeg.input('./test/test_audio.webm')
+print(mydb)
+cursor = mydb.cursor()
+#cursor.execute("CREATE DATABASE tute;")
+#cursor.execute("
 
-ffmpeg.concat(input_video, input_audio, v=1, a=1).output('./processed_folder/finished_video.mp4').run()
+ 
+for x in cursor:
+  print(x)
+
+""" 
+input_video = ffmpeg.input("D:/Coding/Tute/tute/video/test.mp4")
+
+input_audio = ffmpeg.input("D:/Coding/Tute/tute/video/test.mp3")
+
+ffmpeg.concat(input_video, input_audio, v=1, a=1).output("output.mp4").run()
+ """
