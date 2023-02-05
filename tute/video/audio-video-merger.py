931bb1336d2b6313b7cf6e0396f831226aae1d48
Sneha Supriya <codingsneha@gmail.com>
2023-02-05 13:16:20 +0530

    update files

--- tute/video/audio-video-merger.py
+++ tute/video/audio-video-merger.py
@@ -1,7 +1,7 @@
 import ffmpeg
  
-input_video = ffmpeg.input("D:/Coding/Tute/tute/video/test.mp4")
+input_video = ffmpeg.input("D:/Coding/Tute/tute/assets/media/test.mp4")
 
-input_audio = ffmpeg.input("D:/Coding/Tute/tute/video/test.mp3")
+input_audio = ffmpeg.input("D:/Coding/Tute/tute/assets/media/test.mp3")
 
-ffmpeg.concat(input_video, input_audio, v=1, a=1).output("output.mp4").run()
+ffmpeg.concat(input_video, input_audio, v=1, a=1).output("D:/Coding/Tute/tute/assets/media/output.mp4").run()
