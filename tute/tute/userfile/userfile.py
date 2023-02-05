761cd6b0e0db9d3c80501c3c1aafd4c2b2d577c7
Sneha Supriya <codingsneha@gmail.com>
2023-02-05 10:51:56 +0530

    update file upload system

--- tute/tute/userfile/userfile.py
+++ tute/tute/userfile/userfile.py
@@ -2,7 +2,7 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 gauth = GoogleAuth()           
 drive = GoogleDrive(gauth)
-files = ['test.txt']
+files = ["D:/Coding/Tute/tute/tute/userfile/test.txt"]
 for upload_file in files:
 	gfile = drive.CreateFile({'parents': [{'id': '17SP1SxYaZNuQtAduw_AmTgpBqcYOUNdy'}]})
 	gfile.SetContentFile(upload_file)
