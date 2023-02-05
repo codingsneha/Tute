fa1d8c937bf1aa50ec24488a9324e50d1c9352a8
Sneha Supriya <codingsneha@gmail.com>
2023-02-05 10:58:19 +0530

    update files

--- tute/tute/userfile/userfile.py
+++ tute/tute/userfile/userfile.py
@@ -7,3 +7,11 @@ for upload_file in files:
 	gfile = drive.CreateFile({'parents': [{'id': '17SP1SxYaZNuQtAduw_AmTgpBqcYOUNdy'}]})
 	gfile.SetContentFile(upload_file)
 	gfile.Upload()
+
+file_list = drive.ListFile({'q': "'{}' in parents and trashed=false".format('17SP1SxYaZNuQtAduw_AmTgpBqcYOUNdy')}).GetList()
+for file in file_list:
+	print('title: %s, id: %s' % (file['title'], file['id']))
+
+for i, file in enumerate(sorted(file_list, key = lambda x: x['title']), start=1):
+	print('Downloading {} file from GDrive ({}/{})'.format(file['title'], i, len(file_list)))
+	file.GetContentFile(file['title'])
